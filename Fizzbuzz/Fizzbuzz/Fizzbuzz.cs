abebba962e486b09e1d71b9a44074f068e8e497b
guobet <guobet@chrobinson.com>
2020-01-16 14:22:54 +0800

    add case 1

---
+++ Fizzbuzz/Fizzbuzz/Fizzbuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz
+{
+    public class Fizzbuzz
+    {
+        public static int Result(int i)
+        {
+            return 1;
+        }
+    }
+}
---
+++ Fizzbuzz/Fizzbuzz/Test.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fizzbuzz
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestFizzbuzzOne()
+        {
+            Assert.AreEqual(1, Fizzbuzz.Result(1));
+        }
+    }
+}
